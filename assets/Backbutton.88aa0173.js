ba16fe28d5faba75aeb1236d8213391bd715eb86
zeehan-solutions <136564159+zeehan-solutions@users.noreply.github.com>
2025-03-20 14:02:53 +0500

    Add files via upload

---
+++ assets/Backbutton.88aa0173.js
@@ -0,0 +1,6 @@
+import{K as o,j as t,B as n}from"./index.b5bd9156.js";import{x as r}from"./App.6937329a.js";/**
+ * @license @tabler/icons-react v3.29.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var s=r("outline","arrow-left","IconArrowLeft",[["path",{d:"M5 12l14 0",key:"svg-0"}],["path",{d:"M5 12l6 6",key:"svg-1"}],["path",{d:"M5 12l6 -6",key:"svg-2"}]]);const c=({label:l="Back",className:a="mb-1"})=>{const e=o();return t(n,{variant:"tonal",className:`custom-back-button flex items-center gap-2 ${a}`,onClick:()=>e(-1),children:t(s,{size:20,stroke:1.5})})},m=c;export{m as B};
